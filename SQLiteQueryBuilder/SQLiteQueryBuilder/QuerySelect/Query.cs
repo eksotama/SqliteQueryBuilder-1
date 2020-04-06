3f0be354f7d7514617e38a32eae0a779e5ba928b
zhaisw <shuwei.zhai@gmail.com>
2020-04-06 20:04:24 +1200

    Added initialise method.

--- SQLiteQueryBuilder/SQLiteQueryBuilder/QuerySelect/Query.cs
+++ SQLiteQueryBuilder/SQLiteQueryBuilder/QuerySelect/Query.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
 
 namespace SQLiteQueryBuilder.QuerySelect
 {
     public class Query
     {
         private string queryMode = "SELECT";
+        private JObject queryContainer;
 
         public Query()
         {
@@ -19,6 +21,29 @@ namespace SQLiteQueryBuilder.QuerySelect
         {
             if (new string[] {"SELECT", "INSERT", "UPDATE", "DELETE"}.Contains(mode.ToUpper()))
                 queryMode = mode.ToUpper();
+        }
+
+
+        private JObject InitialiseQueryContainer(string mode)
+        {
+            var container = new JObject();
+
+            switch (mode.ToUpper())
+            {
+                case "SELECT":
+
+                    break;
+                case "INSERT":
+                    break;
+                case "UPDATE":
+                    break;
+                case "DELETE":
+                    break;
+                default:
+                    throw new NotImplementedException();
+            }
+
+            return container;
         }
     }
 }
